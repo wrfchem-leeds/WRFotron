9e2c162e9d9b222d25877015865c382563237b92
cemachelen <h.l.burns@leeds.ac.uk>
2020-06-15 11:54:37 +0100

    :pencil2: add in nco check to restart file

--- WRFotron2.0_CEMAC_ARC4/main_restart.bash
+++ WRFotron2.0_CEMAC_ARC4/main_restart.bash
@@ -37,9 +37,33 @@ then
 
 msg "substituting initial data with ${prevRunRstFile}"
 
+# Check nco Version
+ncks --version &> ncover.txt
+ncksvno=$(grep -E '[0-9]' ncover.txt | cut -d" " -f5 | tr -d -c 0-9)
+
+msg "nco version is ${ncksvno}"
+
+# Use correct code depending on version
+if (( ${ncksvno} > 460 )); then
+# listing variables in old (chemistry) and new (met-only) restart files
+ncks -m ${prevRunRstFile} | grep -E ':FieldType' |  cut -f1 -d ':' | tr -d '      ' | sort > chemVarList
+ncks -m ${newRunRstFile} | grep -E ':FieldType' |  cut -f1 -d ':' | tr -d '      ' | sort > metVarList
+else
 # listing variables in old (chemistry) and new (met-only) restart files
 ncks -m ${prevRunRstFile} | grep -E ': type' | cut -f 1 -d ' ' | sed 's/://' | sort > chemVarList
 ncks -m ${newRunRstFile} | grep -E ': type' | cut -f 1 -d ' ' | sed 's/://' | sort  > metVarList
+fi
+
+chemvarsno=$(wc -l chemVarList | awk '{ print $1 }')
+metvarsno=$(wc -l metVarList | awk '{ print $1 }')
+
+if [ $chemvarsno == 0 ]; then
+if [ $chemvarsno == $metvarsno ]; then
+ msg "WARNING chemistry restart file and meteorology restart file have same variables"
+else
+  msg "WARNING chemistry variable list nor created"
+fi
+fi
 
 # determining arrays only in old (chemistry) restart file
 chemVarsArr=( $(awk 'FNR==NR{a[$0]++;next}!a[$0]' metVarList chemVarList) )
@@ -59,7 +83,7 @@ done
 msg "chem"
 
 # do the chem run
-${mpiCommandMain} ./wrf.exe
+${mpiCommandMain} wrf.exe
 
 mkdir chem_out
 mv rsl* chem_out
@@ -96,4 +120,3 @@ do
 done
 
 msg "finished WRF/chem"
-
