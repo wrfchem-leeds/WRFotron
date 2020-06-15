4dff3a033da11d83bba2ac7e1edcaf24dca95b83
Luke Conibear <l.a.conibear@leeds.ac.uk>
2020-06-15 14:51:32 +0100

    Delete concatenate_202.bash

--- WRFotron2.0_CEMAC_ARC4/concatenate_202.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#$ -cwd -V
-#$ -l h_rt=48:00:00
-#$ -pe ib 1
-#$ -l node_type=40core-192G
-#$ -l h_vmem=12G
-
-ncrcat -v XLAT,XLONG,Times,P,PB,PH,PHB,PBLH,RAINC,RAINNC,W,T,T2,U,V,PSFC,ALT,SWDOWN,GSW,GLW,SWUPB,LWUPB,LH,HFX,ZNU,ZNW,pres,TH2,p_sl,pres,U10,V10,rh_2m,QVAPOR,F,QRAIN,HGT,P_TOP,LU_INDEX,AOD550_sfc,PM2_5_DRY,PM10,o3,co,so2,no2,bc_a01,bc_a02,bc_a03,bc_a04,oc_a01,oc_a02,oc_a03,oc_a04,num_a01,num_a02,num_a03,num_a04,water_a01,water_a02,water_a03,water_a04,so4_a01,so4_a02,so4_a03,so4_a04,no3_a01,no3_a02,no3_a03,no3_a04,nh4_a01,nh4_a02,nh4_a03,nh4_a04,asoaX_a01,asoaX_a02,asoaX_a03,asoaX_a04,bsoaX_a01,bsoaX_a02,bsoaX_a03,bsoaX_a04,glysoa_sfc_a01,glysoa_sfc_a02,glysoa_sfc_a03,glysoa_sfc_a04,na_a01,na_a02,na_a03,na_a04,oin_a01,oin_a02,oin_a03,oin_a04 --no_tmp_fl wrfout_d01_2014-01* wrfout_2014_jan_chemopt202.nc
