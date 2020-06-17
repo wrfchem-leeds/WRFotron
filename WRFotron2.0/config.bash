3964b9fcdd44bd85aee87129fd621d25a1b01fd5
Luke Conibear <l.a.conibear@leeds.ac.uk>
2020-06-17 13:37:23 +0100

    Changing to WRFChem4.2

--- WRFotron2.0/config.bash
+++ WRFotron2.0/config.bash
@@ -5,66 +5,66 @@
 # code
 # ------------------------------------------------------------------------------
 # WRFotron
-chainDir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/WRFotron2.0
+chainDir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/WRFotron2.0
 version=0.1
-projectTag=WRFChem4.0.3
+projectTag=WRFChem4.2
 withChemistry=true
 # WPS
-WPSdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/WPS4.0.3
+WPSdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/WPS4.2
 # WRFChem
-WRFdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/WRFChem4.0.3
+WRFdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/WRFChem4.2
 # WRFMeteo
-WRFmeteodir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/WRFMeteo4.0.3
+WRFmeteodir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/WRFMeteo4.2
 # ------------------------------------------------------------------------------
 # preprocessors
 # ------------------------------------------------------------------------------
 # MEGAN
-WRFMEGANdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/megan
+WRFMEGANdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/megan
 # MOZBC
-WRFMOZARTdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/mozbc
+WRFMOZARTdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/mozbc
 # WESLEY/EXOCOLDENS
-WRFmztoolsdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/wes-coldens
+WRFmztoolsdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/wes-coldens
 # ANTHRO_EMISS
-WRFanthrodir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/anthro_emis
+WRFanthrodir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/anthro_emis
 # FIRE_EMISS
-WRFfiredir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/finn/src
+WRFfiredir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/finn/src
 # ------------------------------------------------------------------------------
 # input data
 # ------------------------------------------------------------------------------
 # initial and boundary meteorological data
-metDir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_data/initial_boundary_meteo_ecmwf
-#metDir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_data/initial_boundary_meteo_gfs
+metDir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_data/initial_boundary_meteo_ecmwf
+#metDir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_data/initial_boundary_meteo_gfs
 metInc=6
 # initial and boundary chemistry data (MZ4/CAM-Chem pre 2018, WACCM post 2018)
-MOZARTdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_data/initial_boundary_chem_mz4
-#MOZARTdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_data/initial_boundary_chem_camchem
-#MOZARTdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_data/initial_boundary_chem_waccm
+MOZARTdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_data/initial_boundary_chem_mz4
+#MOZARTdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_data/initial_boundary_chem_camchem
+#MOZARTdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_data/initial_boundary_chem_waccm
 # geography data
 geogDir=/nobackup/wrfchem/WPSGeog4
 landuseDir=modis_landuse_21class_30s/
 # MEGAN input data
-MEGANdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_data/emissions/MEGAN
+MEGANdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_data/emissions/MEGAN
 # anthropogenic emissions - data
-emissDir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_data/emissions/EDGAR-HTAP2/MOZART
+emissDir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_data/emissions/EDGAR-HTAP2/MOZART
 # anthropogenic emissions - input namelist
 emissInpFile=emis_edgarhtap2_mozmos.inp
 # anthropogenic emissions - year the emissions are valid for (for offset calculation)
 emissYear=2010
 # fire emissions from FINN (requires a / at the end)
-fireDir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_data/emissions/FINN/
+fireDir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_data/emissions/FINN/
 # FINN fire emissions input file
 fireInpFile=fire_emis.mozm.inp
 # diurnal cycle code
-WRFemitdir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRF4.0.3_code/WRF_UoM_EMIT
+WRFemitdir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/WRF4.2_code/WRF_UoM_EMIT
 # ------------------------------------------------------------------------------
 # simulation directories
 # ------------------------------------------------------------------------------
 # run folder
-workDir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRFChem4.0.3_test/run
+workDir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/test/run
 # output folder
-archiveRootDir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRFChem4.0.3_test/output
+archiveRootDir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/test/output
 # restart folder
-restartRootDir=/nobackup/username/WRFChem4.0.3_WRFotron2.0_clean/WRFChem4.0.3_test/restart
+restartRootDir=/nobackup/username/WRFChem4.2_WRFotron2.0_clean/test/restart
 # remove run directory after run is finished?
 removeRunDir=false
 # post processing script
