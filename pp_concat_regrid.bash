a04903cc917f1e1290d35b0c5dde6861a6e0bfd2
Luke Conibear <laconibear@gmail.com>
2021-02-17 11:44:40 +0000

    adding check on whether using cemac or personal conda

--- pp_concat_regrid.bash
+++ pp_concat_regrid.bash
@@ -4,6 +4,12 @@
 #$ -pe ib 1
 #$ -l h_vmem=148G
 
-conda activate python3_ncl_nco
+using_cemac_conda=$((module list -t) |& grep -i wrfchemconda | wc -c)
+
+if [[ $using_cemac_conda -ne 0 ]]; then
+    module load wrfchemconda/3.7
+else
+    conda activate python3_ncl_nco 
+fi
 
 python pp_concat_regrid.py
